365980f6fe1b9e7b1c389f9dd6300413716cd8b3
Gustavoph <gusta.coliveira07@gmail.com>
2021-05-17 22:33:45 -0300

    first commit

---
+++ Query/server.js
@@ -0,0 +1,24 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+    # Pontos de entrada da sua API!
+    type Query {
+        ola: String!
+    }
+`
+const resolvers = {
+    Query: {
+        ola() {
+            return 'Olá'
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em  ${url}`)
+})
